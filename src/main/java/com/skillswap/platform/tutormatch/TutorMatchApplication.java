30866b3f8dbda1f1c7d38a8c4af662990cd097db
RodrLH <u202212338@upc.edu.pe>
2024-11-15 12:07:19 -0500

    initial commit

---
+++ src/main/java/com/skillswap/platform/tutormatch/TutorMatchApplication.java
@@ -0,0 +1,43 @@
+package com.skillswap.platform.tutormatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * TutorMatchApplication
+ *
+ * @summary
+ * The main class of the TutorMatch application.
+ * It is responsible for starting the Spring Boot application.
+ * It also enables JPA auditing.
+ *
+ * @since 1.0
+ */
+@EnableJpaAuditing
+@SpringBootApplication
+public class TutorMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorMatchApplication.class, args);
+	}
+
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
+
+}
